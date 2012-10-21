97fb439e76139a54f603cbfeef361df5c23b6e07
hideo55 <hide.o.j55@gmail.com>
2012-10-21 23:55:54 +0900

    Change require perl version

--- lib/FormValidator/Lite/Constraint/Moose.pm
+++ lib/FormValidator/Lite/Constraint/Moose.pm
@@ -1,6 +1,8 @@
 package FormValidator::Lite::Constraint::Moose;
 use strict;
 use warnings;
+use utf8;
+use 5.008003;
 use FormValidator::Lite::Constraint;
 use Any::Moose ( '::Util::TypeConstraints' => [] );
 
